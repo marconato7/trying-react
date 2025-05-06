76e92924078580b9b48e657afe24e89135617d1a
José Antônio <41031492+marconato7@users.noreply.github.com>
2025-05-06 21:42:15 +0000

    Xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+const people = 
+[
+    "Alice",
+    "Bob",
+    "Jane",
+    "John",
+];
+
 function Header({ name })
 {
     return (
         <header>Hello, {name}!</header>
+    );
+}
+
+function Main({ people })
+{
+    return (
+        <ul>
+            { people.map(person => <li>{ person }</li>) }
+        </ul>
     );
 }
 
@@ -10,7 +27,7 @@ function App()
     return (
         <div>
             <Header name="Alice" />
-            <main>main</main>
+            <Main people={people} />
         </div>
     );
 }
